d0a347d4d654fea29ddc0fc4ed695787aff82bc7
Tyler Kaminski <durcor@disroot.org>
2021-05-05 21:10:36 -0400

    Implement neural network inference and add README

---
+++ README.md
@@ -0,0 +1,13 @@
+# predict-forest-fires
+Group research project for EN250 (Quantitative Biology) at Stevens Institute of Technology concerning predicting forest fire likelihood using neural networks.
+
+## Dependencies
+- [python](https://www.python.org/downloads/)
+- [scikit-learn](https://scikit-learn.org/stable/install.html)
+- [pandas](https://pandas.pydata.org/getting_started.html)
+
+## Usage
+```sh
+cd src
+./predict_forest_fires.py
+```
--- src/predict_forest_fires.py
+++ src/predict_forest_fires.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 from pandas import read_csv
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 # Goal:
 # - predict values in the 1st or 2nd column (fire severity classes) from
@@ -11,15 +14,28 @@ from sklearn.neural_network import MLPClassifier
 # severity: Forest Fire Severity
 # BA: Basal Area Loss
 # Range from 1 - 9
-data = read_csv("subset_1percent.csv")
-severity_classes = data['Severity_classes']
-ba_classes = data['BA_classes']
-print(severity_classes)
-print(ba_classes)
+small_data = read_csv("../data/subset_1percent.csv")
+medium_data = read_csv("../data/subset_5percent.csv")
+big_data = read_csv("../data/subset_10percent.csv")
 
-# Predict Severity classes
-clf = MLPClassifier()
-severity_neural_net = clf.fit(data, severity_classes)
+# ~ 52% Accurate
+X = small_data.loc[:, "FIRST_RETURNS_int_P01_2m+":"FIRST_RETURNS_int_P99_2m+"]
+# ~ 47% Accurate
+# X = small_data.iloc[:, 2:]
 
-# Predict BA classes
-ba_neural_net = clf.fit(data, ba_classes)
+def predict(y, class_type):
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    nn = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+    nn.fit(X_train, y_train.values.ravel())
+
+    print("Accuracy of predictions for", class_type, ":",
+          accuracy_score(y_test, nn.predict(X_test)))
+
+predict(small_data["Severity_classes"], "Severity Classes")
+predict(small_data["BA_classes"], "BA Classes")
