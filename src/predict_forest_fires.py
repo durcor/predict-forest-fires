3d49931c278fcfd3c8e62b17a91639e9922a29cb
Tyler Kaminski <durcor@disroot.org>
2021-05-06 00:56:55 -0400

    Add broader accuracy score metric, optimize neural net

--- src/predict_forest_fires.py
+++ src/predict_forest_fires.py
@@ -5,22 +5,33 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Goal:
-# - predict values in the 1st or 2nd column (fire severity classes) from
-# other columns that contain remote sensing parameters for the different
-# forest patches (every row - 1 forest patch).
+
+def broad_accuracy_score(y_true, y_pred):
+    y_true = y_true.tolist()
+    y_pred = y_pred.tolist()
+    sum = 0
+    for i in range(len(y_true)):
+        if y_pred[i] in [y_true[i], y_true[i] + 1, y_true[i] - 1]:
+            sum += 1
+    return sum / len(y_true)
+
 
 # Vocabulary:
-# severity: Forest Fire Severity
+# Severity: Forest Fire Severity
 # BA: Basal Area Loss
 # Range from 1 - 9
-small_data = read_csv("../data/subset_1percent.csv")
-medium_data = read_csv("../data/subset_5percent.csv")
-big_data = read_csv("../data/subset_10percent.csv")
 
-X = small_data.iloc[:, 2:]
+data = read_csv("../data/subset_1percent.csv")
+# data = read_csv("../data/subset_5percent.csv")
+# data = read_csv("../data/subset_10percent.csv")
 
+# Read all recorded parameters into the variable to be trained/tested.
+X = data.iloc[:, 2:]
+
+
+# Predict BA and Severity classes.
 def predict(y, class_type):
+    # Partition samples to train and test.
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     scaler = StandardScaler()
@@ -28,15 +39,27 @@ def predict(y, class_type):
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
 
-    nn = MLPClassifier(hidden_layer_sizes=(5,), max_iter=10000, learning_rate='adaptive')
+    # NOTE: Tweak the parameters of this to improve prediction.
+    nn = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10000)
+    # Train the neural network.
     nn.fit(X_train, y_train)
 
+    # Predict the class of the tested sample.
     y_pred = nn.predict(X_test)
+
     print(class_type)
+    # Print out samples for user to check for differences and overall accuracy
+    # of prediction.
     print("Tested sample:   ", y_test.tolist()[:40])
     print("Predicted sample:", y_pred.tolist()[:40])
-    print("Accuracy of predictions:", accuracy_score(y_test, y_pred))
+    accuracy = round(accuracy_score(y_test, y_pred) * 100, 2)
+    broad_accuracy = round(broad_accuracy_score(y_test, y_pred) * 100, 2)
+    print("Exact accuracy of predictions: " + str(accuracy) + '%')
+    print("Broad accuracy of predictions: " + str(broad_accuracy) + '%')
+    # TODO: Find a more broad accuracy measurement by allowing for a margin of
+    # error of +/-1 for each predicted class.
     print()
 
-predict(small_data["Severity_classes"], "Severity Classes")
-predict(small_data["BA_classes"], "BA Classes")
+
+predict(data["Severity_classes"], "Severity Classes")
+predict(data["BA_classes"], "BA Classes")
